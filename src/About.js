925167c1ad2d3d426e7a08f6ae93f98d2ba336bb
Jaume <ballestertomasj@gmail.com>
2022-01-20 11:41:33 +0100

    about

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about component__space">
+      <h1>it works</h1>
+    </div>
+  );
+}
+
+export default About;
