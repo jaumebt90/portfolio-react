8e6b1ee51db8300d4675212d0d0e44e5e4e8805e
Jaume <ballestertomasj@gmail.com>
2022-01-20 14:06:58 +0100

    pics

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Project.css";
 import project1 from "./img/project1.jpeg";
+import project2 from "./img/project2.png";
+import project3 from "./img/project3.jpeg";
+import project4 from "./img/project4.jpeg";
+import project5 from "./img/project5.png";
+import project6 from "./img/project6.png";
 
 function Project() {
   return (
@@ -17,6 +22,91 @@ function Project() {
               <div className="project__img__box pointer relative">
                 <div className="project__img__box">
                   <img src={project1} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">blablablablabla</h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__img__box pointer relative">
+                <div className="project__img__box">
+                  <img src={project2} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">blablablablabla</h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__img__box pointer relative">
+                <div className="project__img__box">
+                  <img src={project3} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">blablablablabla</h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__img__box pointer relative">
+                <div className="project__img__box">
+                  <img src={project4} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">blablablablabla</h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__img__box pointer relative">
+                <div className="project__img__box">
+                  <img src={project5} alt="" className="project__img" />
+                </div>
+                <div className="mask__effect"></div>
+              </div>
+              <div className="project__meta absolute">
+                <h5 className="project__text">Development</h5>
+                <h4 className="project__text">blablablablabla</h4>
+                <a href="#" className="project__btn">
+                  View Details
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col__3">
+            <div className="project__box pointer relative">
+              <div className="project__img__box pointer relative">
+                <div className="project__img__box">
+                  <img src={project6} alt="" className="project__img" />
                 </div>
                 <div className="mask__effect"></div>
               </div>
