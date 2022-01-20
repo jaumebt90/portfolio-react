b01ac399326967a1f406bae4c86c70d5f1a8974f
Jaume <ballestertomasj@gmail.com>
2022-01-20 13:01:18 +0100

    project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import About from "./About";
 import "./App.css";
 import Home from "./Home";
+import Project from "./Project";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Project />
     </div>
   );
 }
