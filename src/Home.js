4fb4ad6003a64a2309f33ea8bc0432ff642997d5
Jaume <ballestertomasj@gmail.com>
2022-01-20 11:11:36 +0100

    home

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,29 @@ function Home() {
     <div className="home">
       <div className="home__bg">
         <div className="header d__flex align__items__center pxy__30">
-          <div className="logo">
+          {/* <div className="logo">
             <img src={logo} alt="" />
-          </div>
-          <div className="navigation">
-            <ul></ul>
+          </div> */}
+          <div className="navigation pxy__30">
+            <ul className="navbar d__flex">
+              <a href="#Home">
+                <li className="nav__items mx__15">Home</li>
+              </a>
+              <a href="#About">
+                <li className="nav__items mx__15">About</li>
+              </a>
+              <a href="#Projects">
+                <li className="nav__items mx__15">Projects</li>
+              </a>
+              <a href="#Contact">
+                <li className="nav__items mx__15">Contact</li>
+              </a>
+            </ul>
           </div>
         </div>
+        {/* HOME CONTENT */}
+        <div className="container"></div>
+        <div className="home__content"></div>
       </div>
     </div>
   );
