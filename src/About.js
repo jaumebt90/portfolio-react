9eaa0654f796642b762edb9de208f882e40df67c
Jaume <ballestertomasj@gmail.com>
2022-01-20 11:49:23 +0100

    about pic

--- src/About.js
+++ src/About.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./About.css";
+import me from "./img/me.png";
 
 function About() {
   return (
     <div className="about component__space">
-      <h1>it works</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col__2">
+            <img src={me} alt="" className="about__img" />
+          </div>
+          <div className="col__2"></div>
+        </div>
+      </div>
     </div>
   );
 }
