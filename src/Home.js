f75c5be9f46a1661287c71885813df7d14686319
Jaume <ballestertomasj@gmail.com>
2022-01-20 10:37:33 +0100

    img

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Home.css";
+import logo from "./img/logo.jpeg";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="home__bg">
+        <div className="header d__flex align__items__center pxy__30">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          <div className="navigation">
+            <ul></ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
